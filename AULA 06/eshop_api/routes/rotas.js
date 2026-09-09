df021ecc21813f8bb02507cbc1c297b519dcf8db
Matheus_WD_2006 <matheusditz.pf028@academico.ifsul.edu.br>
2026-09-09 08:45:42 -0300

    aula dia 03-09, api com node e banco de dados

---
+++ AULA 06/eshop_api/routes/rotas.js
@@ -0,0 +1,11 @@
+const { Router } = require('express');
+
+const {
+    rotasCategorias
+} = require('./rotasCategorias');
+
+const rotas = new Router();
+
+rotas.use(rotasCategorias);
+
+module.exports = rotas;
