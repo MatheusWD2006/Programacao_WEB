382b3ba6db1632819e226d76e84f45ca5b986a87
Matheus Witte Ditz <matheusditz.pf028@academico.ifsul.edu.br>
2026-09-19 17:21:49 -0300

    aplicação dos passos 2 e 3 da aula do dia 17/09

--- AULA 06 e 07/eshop-frontend/src/components/view/categoria/Categoria.jsx
+++ AULA 06 e 07/eshop-frontend/src/components/view/categoria/Categoria.jsx
@@ -7,6 +7,7 @@ import {
   cadastraCategoriaAPI,
 } from "../../../services/CategoriaServico";
 import Tabela from "./Tabela";
+import Formulario from './Formulario';
 
 function Categoria() {
   const [alerta, setAlerta] = useState({ status: "", message: "" });
@@ -28,18 +29,89 @@ function Categoria() {
     recuperaCategorias();
   }, []);
 
-  return (
-    <CategoriaContext.Provider
-      value={{
-        alerta,
-        setAlerta,
-        listaObjetos,
-        remover,
-      }}
-    >
-      <Tabela />
+  // Novos estados e métodos
+const [editar, setEditar] = useState(false);
+const [exibirForm, setExibirForm] = useState(false);
+
+const [objeto, setObjeto] = useState({
+    codigo: "", 
+    nome: "", 
+    descricao: "", 
+    sigla: ""
+});
+
+const novoObjeto = () => {
+    setEditar(false);
+    setAlerta({ status: "", message: "" });
+    setObjeto({
+        codigo: 0,
+        nome: ""
+    });
+    setExibirForm(true);
+}
+
+const editarObjeto = async codigo => {
+    setObjeto(await getCategoriaPorCodigoAPI(codigo));
+    setEditar(true);
+    setAlerta({ status: "", message: "" });
+    setExibirForm(true);
+}
+
+const acaoCadastrar = async e => {
+    e.preventDefault();
+    const metodo = editar ? "PUT" : "POST";
+
+    try {
+        let retornoAPI = await cadastraCategoriaAPI(objeto, metodo);
+
+        setAlerta({
+            status: retornoAPI.status,
+            message: retornoAPI.message
+        });
+
+        setObjeto(retornoAPI.objeto);
+
+        if (!editar) {
+            setEditar(true);
+        }
+    } catch (err) {
+        console.error(err.message);
+    }
+
+    recuperaCategorias();
+}
+
+const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+
+    setObjeto({
+        ...objeto,
+        [name]: value
+    });
+}
+
+// Adicionando no value do Provider os novos estados e métodos
+// para uso dos componentes filhos
+return (
+    <CategoriaContext.Provider value={
+        {
+            listaObjetos,
+            alerta,
+            remover,
+            objeto,
+            editarObjeto,
+            acaoCadastrar,
+            handleChange,
+            novoObjeto,
+            exibirForm,
+            setExibirForm
+        }
+    }>
+        <Tabela />
+        <Formulario />
     </CategoriaContext.Provider>
-  );
+);
 }
 
 export default Categoria;
---
+++ AULA 06 e 07/eshop-frontend/src/components/view/categoria/Formulario.jsx
@@ -0,0 +1,136 @@
+import { useContext, useState } from 'react';
+
+import Alerta from '../../common/Alerta';
+import CategoriaContext from './CategoriaContext';
+
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import Form from 'react-bootstrap/Form';
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+
+function Formulario() {
+
+    const {
+        objeto,
+        handleChange,
+        acaoCadastrar,
+        alerta,
+        exibirForm,
+        setExibirForm
+    } = useContext(CategoriaContext);
+
+    const [validated, setValidated] = useState(false);
+
+    const handleSubmit = (event) => {
+
+        const form = event.currentTarget;
+
+        if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
+
+        setValidated(true);
+
+        if (form.checkValidity() === true) {
+            acaoCadastrar(event);
+        }
+    };
+
+    return (
+        <Modal
+            show={exibirForm}
+            onHide={() => setExibirForm(false)}
+        >
+            <Modal.Header closeButton>
+                <Modal.Title>Categoria</Modal.Title>
+            </Modal.Header>
+
+            <Form
+                id="formulario"
+                onSubmit={handleSubmit}
+                noValidate
+                validated={validated}
+            >
+
+                <Modal.Body>
+
+                    <Container>
+                        <Row>
+
+                            <Alerta alerta={alerta} />
+
+                            <Col xs={12} md={12}>
+                                <FloatingLabel
+                                    controlId="txtCodido"
+                                    label="Código"
+                                    className="mb-3"
+                                >
+                                    <Form.Control
+                                        type="text"
+                                        readOnly
+                                        name="codigo"
+                                        value={objeto.codigo}
+                                        onChange={handleChange}
+                                    />
+                                </FloatingLabel>
+                            </Col>
+
+                            <Col xs={12} md={12}>
+                                <FloatingLabel
+                                    controlId="txtNome"
+                                    label="Nome"
+                                    className="mb-3"
+                                >
+                                    <Form.Control
+                                        type="text"
+                                        required
+                                        name="nome"
+                                        value={objeto.nome}
+                                        onChange={handleChange}
+                                        placeholder="Informe o nome"
+                                    />
+
+                                    <Form.Control.Feedback>
+                                        Campo nome OK!
+                                    </Form.Control.Feedback>
+
+                                    <Form.Control.Feedback type="invalid">
+                                        Informe o nome
+                                    </Form.Control.Feedback>
+
+                                </FloatingLabel>
+                            </Col>
+
+                        </Row>
+                    </Container>
+
+                </Modal.Body>
+
+                <Modal.Footer>
+
+                    <Button
+                        variant="secondary"
+                        onClick={() => setExibirForm(false)}
+                    >
+                        Fechar
+                    </Button>
+
+                    <Button
+                        variant="success"
+                        type="submit"
+                    >
+                        Salvar <i className="bi bi-save"></i>
+                    </Button>
+
+                </Modal.Footer>
+
+            </Form>
+        </Modal>
+    );
+}
+
+export default Formulario;
